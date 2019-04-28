d6fca2410d248d53a06cc2ab923e9a5a745b7c33
samuele794 <samuelebruschi794@gmail.com>
2019-04-28 22:59:05 +0200

    Aggiornato Sito Web Aggiornato Versionamento bot sul gradle in 1.2.0

--- src/test/java/testing/StartTest.java
+++ src/test/java/testing/StartTest.java
@@ -1,45 +1,26 @@
 package testing;
 
 import com.google.gson.Gson;
-import command.real.sound.PlayerControlCommand;
-import interfaces.SQLiteInterfaces;
+import interfaces.PostgreSQLInterface;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.Permission;
 
 import javax.security.auth.login.LoginException;
 import java.io.IOException;
-import java.security.GeneralSecurityException;
 
 public class StartTest {
 
 	public static Gson gson = new Gson();
 	public static JDA jda;
 
-	public static void main(String args[]) throws GeneralSecurityException, IOException {
+	public static void main(String[] args) throws IOException {
 
-//        Boss boss1 = new Boss(new String[]{"Kutum", "Kzarka"}, "00:15");
-//        Boss boss2 = new Boss(new String[]{"Kzarka"}, "02:00");
-//
-//        ArrayList<Boss> arrayList = new ArrayList<>();
-//        arrayList.add(boss1);
-//        arrayList.add(boss2);
-//
-//        Giorno lunedi = new Giorno(Giorno.Giorni.LUNEDI, arrayList);
-//        Giorno martedi = new Giorno(Giorno.Giorni.MARTEDI, arrayList);
-//
-//        ArrayList<Giorno> lg = new ArrayList<>();
-//        lg.add(lunedi);
-//        lg.add(martedi);
-//
-//        Gson gson = new Gson();
-//        gson.toJson(lg.toArray());
-
-		SQLiteInterfaces.initializeDB();
-
+//		SQLiteInterfaces.initializeDB();
+		PostgreSQLInterface.initializeDB();
 
 		try {
-			jda = new JDABuilder("").build();
+			jda = new JDABuilder("NTMwMzA1MjEzNDkyNDk0MzM4.D3RW1A.IXqW6JxGvSrBHpstTXuZtVOyCO4").build();
 		} catch (LoginException e) {
 			e.printStackTrace();
 			return;
@@ -50,7 +31,6 @@ public class StartTest {
 
 		jda.addEventListener(new TestCommand());
 //		jda.addEventListener(new PrivateMessage());
-		jda.addEventListener(new PlayerControlCommand());
 
 		System.out.println(jda.asBot().getInviteUrl(Permission.ADMINISTRATOR));
 
--- src/test/java/testing/TestCommand.java
+++ src/test/java/testing/TestCommand.java
@@ -1,17 +1,20 @@
 package testing;
 
 import command.pattern.ControlCommand;
-import net.dv8tion.jda.core.entities.Message;
+import interfaces.PostgreSQLInterface;
+import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+import java.awt.*;
 
 public class TestCommand extends ListenerAdapter {
 	@Override
 	public void onMessageReceived(MessageReceivedEvent event) {
 		if (event.getAuthor().isBot()) return;
 		// We don't want to respond to other bot accounts, including ourself
-		Message message = event.getMessage();
-		String content = message.getContentRaw();
+//		Message message = event.getMessage();
+//		String content = message.getContentRaw();
 		// getContentRaw() is an atomic getter
 		// getContentDisplay() is a lazy getter which modifies the content for e.g. console view (strip discord formatting)
         /*if (content.equals("!ping")) {
@@ -19,10 +22,24 @@ public class TestCommand extends ListenerAdapter {
             channel.sendMessage("Pong!").queue(); // Important to call .queue() on the RestAction returned by sendMessage(...)
         }*/
 
+
 		if (ControlCommand.controlCommand(event, "ping")) {
 
+			EmbedBuilder messageEmbed = new EmbedBuilder();
+
+			messageEmbed
+					.setDescription("\n" +
+							"\n" +
+							"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque hendrerit faucibus lorem, in faucibus elit tempor in. Vestibulum fringilla mi erat, eget fringilla massa mollis sit amet. Fusce aliquam dolor laoreet, congue turpis a, iaculis eros. Donec ullamcorper ultricies justo quis bibendum. Aenean porttitor vehicula nisi quis venenatis. Donec id felis mauris. Mauris rhoncus a mi eget ultrices.\n" +
+							"\nm eu nisi eu egestas. Nulla tincidunt enim ac eam. Curabitur suscipit magna id turpis porta. ").setColor(new Color(132, 197, 251))
+					.setThumbnail(event.getJDA().getSelfUser().getAvatarUrl());
+			event.getChannel().sendMessage(messageEmbed.build()).queue();
+
+		//			event.getGuild().getRoles();
 //			  channel.sendMessage("Pong!").queue();
-		}
+
+
+			}
 	}
 
 }
